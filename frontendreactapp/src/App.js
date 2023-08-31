c7a1da2d7618b5fea93a303e30fc43d451d96dd9
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-08-31 22:49:19 +0400

    Project's added structure

---
+++ frontendreactapp/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+
+function App() {
+  const [posts, setPost] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPost(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
+    </div>
+  );
+}
+
+export default App;
